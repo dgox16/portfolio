d022516ddfe4de3d70b435fe144d2f6be1e99b29
Diego <dgox16@gmail.com>
2024-02-06 14:29:27 -0600

    Fix font size of icon about me

--- src/components/AboutMe/ElementGrid.jsx
+++ src/components/AboutMe/ElementGrid.jsx
@@ -6,7 +6,7 @@ export const ElementGrid = ({ children, headElement, colorStyle }) => {
         4: "bg-blue",
     };
 
-    const styles = `w-16 h-16 md:w-20 md:h-20 mb-7 ${st[colorStyle]} text-base rounded-full flex justify-center m-auto items-center text-center font-bold text-2xl md:text-5xl shadow-xl`;
+    const styles = `w-16 h-16 md:w-20 md:h-20 mb-7 ${st[colorStyle]} text-base rounded-full flex justify-center m-auto items-center text-center font-bold text-4xl md:text-5xl shadow-xl`;
     return (
         <div className="flex flex-col">
             <div className="flex justify-center items-center">
