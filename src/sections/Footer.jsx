0d095afa8e86234fe0db110538d4534c67cbcc7b
Diego <dgox16@gmail.com>
2024-02-28 16:48:23 -0600

    Better footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -2,10 +2,12 @@ export const Footer = () => {
     return (
         <div className="bg-crust flex justify-center">
             <div className=" w-recommend text-center">
-                <div className="py-3 text-text">
-                    © 2024 Diego Armando Gómez Martínez. <span> | </span> Casi todos los derechos
+                <p className="py-3 text-text text-sm xs:text-[16px]">
+                    © 2024 Diego Armando Gómez Martínez.{" "}
+                    <span className="hidden sm:inline"> | </span>
+                    <br aria-hidden={true} className="block sm:hidden" /> Casi todos los derechos
                     reservados
-                </div>
+                </p>
             </div>
         </div>
     );
