9e58a2a118da99928d3a175f6066eedd9fbc9089
Diego <dgox16@gmail.com>
2024-02-28 17:12:38 -0600

    Add english in the footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,12 +1,17 @@
+import { useLanguageStore } from "../store/languageStore";
+
 export const Footer = () => {
+    const { inSpanish } = useLanguageStore();
     return (
         <div className="bg-crust flex justify-center">
             <div className=" w-recommend text-center">
                 <p className="py-3 text-text text-sm xs:text-[16px]">
                     © 2024 Diego Armando Gómez Martínez.{" "}
                     <span className="hidden sm:inline"> | </span>
-                    <br aria-hidden={true} className="block sm:hidden" /> Casi todos los derechos
-                    reservados
+                    <br aria-hidden={true} className="block sm:hidden" />{" "}
+                    {inSpanish
+                        ? "Casi todos los derechos reservados."
+                        : "Almost all rights reserved."}
                 </p>
             </div>
         </div>
