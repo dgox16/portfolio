64263483bf86402dc26c1330810d81971000f445
Diego <dgox16@gmail.com>
2024-03-05 09:25:24 -0600

    The button to download CV works

--- src/components/Header/HeaderButtons/ButtonDownloadCv.jsx
+++ src/components/Header/HeaderButtons/ButtonDownloadCv.jsx
@@ -5,6 +5,16 @@ import { motion } from "framer-motion";
 
 export const ButtonDownloadCv = () => {
     const { inSpanish } = useLanguageStore();
+
+    const handleDownload = () => {
+        const url = "/Resume-DiegoArmando.pdf";
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", "cv.pdf");
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
     return (
         <div className="flex justify-center lg:justify-normal col-span-2">
             <motion.button
@@ -13,6 +23,7 @@ export const ButtonDownloadCv = () => {
                 }}
                 transition={{ type: "spring", stiffness: 400, damping: 10 }}
                 whileTap={{ scale: 0.9 }}
+                onClick={handleDownload}
                 type="button"
                 className="w-4/5 lg:w-full text-base  flex items-center justify-center font-display text-xl font-bold rounded-xl h-12 bg-gradient-to-tr from-blue via-mauve to-pink"
             >
