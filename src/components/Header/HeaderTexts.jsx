37af050a7688e95e2db68bf74820268cb87fa3c6
Diego <dgox16@gmail.com>
2024-04-18 10:02:44 -0600

    Se aumento el tamaño del encabezado

--- src/components/Header/HeaderTexts.jsx
+++ src/components/Header/HeaderTexts.jsx
@@ -2,7 +2,7 @@ export const TextTitleHeader = ({ children }) => {
     return (
         <h1
             className={
-                "text-[22px] xs:text-[25px] sm:text-[40px] md:text-[50px] lg:text-[45px] xl:text-[55px] font-extrabold text-text"
+                "text-[22px] xs:text-[28px] sm:text-[40px] md:text-[50px] lg:text-[45px] xl:text-[55px] font-extrabold text-text"
             }
         >
             {children}
@@ -12,7 +12,7 @@ export const TextTitleHeader = ({ children }) => {
 
 export const TextTitleGradientHeader = ({ children }) => {
     return (
-        <h1 className="text-[22px] xs:text-[25px] sm:text-[40px] whitespace-pre-wrap bg-gradient-to-tr from-blue via-mauve to-pink text-transparent bg-clip-text md:text-[50px] lg:text-[45px] xl:text-[55px] font-extrabold">
+        <h1 className="text-[22px] xs:text-[28px] sm:text-[40px] whitespace-pre-wrap bg-gradient-to-tr from-blue via-mauve to-pink text-transparent bg-clip-text md:text-[50px] lg:text-[45px] xl:text-[55px] font-extrabold">
             {children}
         </h1>
     );
