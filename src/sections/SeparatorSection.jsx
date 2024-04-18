84b761e9953f431c16f382ed0ac1b7aec875b772
Diego <dgox16@gmail.com>
2024-04-18 11:28:51 -0600

    Mas separacion entre separador

--- src/sections/SeparatorSection.jsx
+++ src/sections/SeparatorSection.jsx
@@ -1,7 +1,7 @@
 export const SeparatorSection = () => {
     return (
         <div className="flex justify-center">
-            <div className="w-recommend my-6 md:my-14 bg-mantle rounded-lg h-[4px]" />
+            <div className="w-recommend my-8 md:my-14 bg-mantle rounded-lg h-[4px]" />
         </div>
     );
 };
